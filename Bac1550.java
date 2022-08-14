3457ac2d8b70d3591f4c49b8830fd11c316059eb
Kimc <32549359+Lilpha@users.noreply.github.com>
2022-08-15 01:49:12 +0900

    BaekJoon1550

---
+++ Bac1550.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Bac1550 {
+	public static void main(String[]args) {
+		//https://www.acmicpc.net/submit/1550
+		Scanner sc = new Scanner(System.in);
+		String num = sc.next();//String형태로 받아서 ParseInt사용예정
+		
+		int number = Integer.parseInt(num,16); //number에 num을 정수형 16진수변환
+		System.out.println(number);
+		
+		
+	}
+}
